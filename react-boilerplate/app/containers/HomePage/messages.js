9546824319e0e564153569d2bb95dd95bd202c40
Christopher Jensen <christopher.jensen@daugherty.com>
2019-09-30 11:51:22 -0500

    Remove default example

--- react-boilerplate/app/containers/HomePage/messages.js
+++ react-boilerplate/app/containers/HomePage/messages.js
@@ -1,31 +1,15 @@
 /*
  * HomePage Messages
  *
- * This contains all the text for the HomePage component.
+ * This contains all the text for the HomePage container.
  */
 import { defineMessages } from 'react-intl';
 
-export const scope = 'boilerplate.containers.HomePage';
+export const scope = 'app.containers.HomePage';
 
 export default defineMessages({
-  startProjectHeader: {
-    id: `${scope}.start_project.header`,
-    defaultMessage: 'Hi',
-  },
-  startProjectMessage: {
-    id: `${scope}.start_project.message`,
-    defaultMessage: 'Maybe this will udpate?',
-  },
-  trymeHeader: {
-    id: `${scope}.tryme.header`,
-    defaultMessage: 'No U',
-  },
-  trymeMessage: {
-    id: `${scope}.tryme.message`,
-    defaultMessage: 'Show Github repositories by',
-  },
-  trymeAtPrefix: {
-    id: `${scope}.tryme.atPrefix`,
-    defaultMessage: '@',
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'This is the HomePage container!',
   },
 });
